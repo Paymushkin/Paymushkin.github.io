a110927d6f7996154eb7f97d69e9f00ba81cd6d2
Andrew Paymushkin <paymushkin_a@bk.ru>
2023-12-22 23:18:08 +0300

    mobile version

--- diana/src/js/files/sliders.js
+++ diana/src/js/files/sliders.js
@@ -108,12 +108,12 @@ function initSliders() {
 			},
 
 			// Брейкпоинты
-			/*
+
 			breakpoints: {
 				320: {
 					slidesPerView: 1,
 					spaceBetween: 0,
-					autoHeight: true,
+					// autoHeight: true,
 				},
 				768: {
 					slidesPerView: 2,
@@ -124,11 +124,10 @@ function initSliders() {
 					spaceBetween: 20,
 				},
 				1268: {
-					slidesPerView: 4,
+					slidesPerView: 3,
 					spaceBetween: 30,
 				},
 			},
-			*/
 			// События
 			// on: {
 			// 	init: function(swiper) {
@@ -201,8 +200,8 @@ function initSliders() {
 			breakpoints: {
 				320: {
 					slidesPerView: 1,
-					spaceBetween: 0,
-					autoHeight: true,
+					spaceBetween: 40,
+					// autoHeight: true,
 				},
 				576: {
 					slidesPerView: 2,
@@ -237,98 +236,98 @@ function initSliders() {
 		});
 	}
 
-	if (window.innerWidth <= 576) {
+	// if (window.innerWidth <= 576) {
 
-		if (document.querySelector('.news__slider') ) { // Указываем скласс нужного слайдера
-			// Создаем слайдер
-			new Swiper('.news__slider', { // Указываем скласс нужного слайдера
-				// Подключаем модули слайдера
-				// для конкретного случая
-				modules: [Autoplay, Navigation],
-				observer: true,
-				observeParents: true,
-				slidesPerView: 1,
-				spaceBetween: 0,
-				// autoHeight: true,
-				speed: 800,
+	// 	if (document.querySelector('.news__slider') ) { // Указываем скласс нужного слайдера
+	// 		// Создаем слайдер
+	// 		new Swiper('.news__slider', { // Указываем скласс нужного слайдера
+	// 			// Подключаем модули слайдера
+	// 			// для конкретного случая
+	// 			modules: [Autoplay, Navigation],
+	// 			observer: true,
+	// 			observeParents: true,
+	// 			slidesPerView: 1,
+	// 			spaceBetween: 0,
+	// 			// autoHeight: true,
+	// 			speed: 800,
 	
-				//touchRatio: 0,
-				//simulateTouch: false,
-				loop: true,
-				//preloadImages: false,
-				//lazy: true,
-				parallax: true,
+	// 			//touchRatio: 0,
+	// 			//simulateTouch: false,
+	// 			loop: true,
+	// 			//preloadImages: false,
+	// 			//lazy: true,
+	// 			parallax: true,
 	
 				
-				// Эффекты
-				effect: 'fade',
-				autoplay: {
-					delay: 3000,
-					disableOnInteraction: false,
-				},
+	// 			// Эффекты
+	// 			effect: 'fade',
+	// 			autoplay: {
+	// 				delay: 3000,
+	// 				disableOnInteraction: false,
+	// 			},
 				
 	
-				// Пагинация
+	// 			// Пагинация
 				
-				// pagination: {
-				// 	el: '.swiper-pagination',
-				// 	clickable: true,
-				// 	type: "fraction",
-				// },
+	// 			// pagination: {
+	// 			// 	el: '.swiper-pagination',
+	// 			// 	clickable: true,
+	// 			// 	type: "fraction",
+	// 			// },
 				
 	
-				// Скроллбар
-				/*
-				scrollbar: {
-					el: '.swiper-scrollbar',
-					draggable: true,
-				},
-				*/
+	// 			// Скроллбар
+	// 			/*
+	// 			scrollbar: {
+	// 				el: '.swiper-scrollbar',
+	// 				draggable: true,
+	// 			},
+	// 			*/
 	
-				// Кнопки "влево/вправо"
-				navigation: {
-					prevEl: '.news .navigation .prev',
-					nextEl: '.news .navigation .next',
-				},
+	// 			// Кнопки "влево/вправо"
+	// 			navigation: {
+	// 				prevEl: '.news .navigation .prev',
+	// 				nextEl: '.news .navigation .next',
+	// 			},
 	
-				// Брейкпоинты
-				/*
-				breakpoints: {
-					320: {
-						slidesPerView: 1,
-						spaceBetween: 0,
-						autoHeight: true,
-					},
-					768: {
-						slidesPerView: 2,
-						spaceBetween: 20,
-					},
-					992: {
-						slidesPerView: 3,
-						spaceBetween: 20,
-					},
-					1268: {
-						slidesPerView: 4,
-						spaceBetween: 30,
-					},
-				},
-				*/
-				// События
-				// on: {
-				// 	init: function(swiper) {
-				// 		const allSlides = document.querySelector(".fraction-controll__all");
-				// 		const allSlidesItems = document.querySelectorAll(".slide-main-block:not(.swiper-slide-duplicate)");
+	// 			// Брейкпоинты
+	// 			/*
+	// 			breakpoints: {
+	// 				320: {
+	// 					slidesPerView: 1,
+	// 					spaceBetween: 0,
+	// 					autoHeight: true,
+	// 				},
+	// 				768: {
+	// 					slidesPerView: 2,
+	// 					spaceBetween: 20,
+	// 				},
+	// 				992: {
+	// 					slidesPerView: 3,
+	// 					spaceBetween: 20,
+	// 				},
+	// 				1268: {
+	// 					slidesPerView: 4,
+	// 					spaceBetween: 30,
+	// 				},
+	// 			},
+	// 			*/
+	// 			// События
+	// 			// on: {
+	// 			// 	init: function(swiper) {
+	// 			// 		const allSlides = document.querySelector(".fraction-controll__all");
+	// 			// 		const allSlidesItems = document.querySelectorAll(".slide-main-block:not(.swiper-slide-duplicate)");
 	
-				// 		allSlides.textContent = allSlidesItems.length < 10 ? `0${allSlidesItems.length}` : allSlidesItems.length;
-				// 	},
-				// 	slideChange: function(swiper) {
-				// 		const currentSlide = document.querySelector(".fraction-controll__current");
-				// 		currentSlide.textContent = (swiper.realIndex + 1) < 10 ? `0${swiper.realIndex + 1}` : swiper.realIndex + 1;
-				// 	}
-				// }
-			});
-		}
-	}
+	// 			// 		allSlides.textContent = allSlidesItems.length < 10 ? `0${allSlidesItems.length}` : allSlidesItems.length;
+	// 			// 	},
+	// 			// 	slideChange: function(swiper) {
+	// 			// 		const currentSlide = document.querySelector(".fraction-controll__current");
+	// 			// 		currentSlide.textContent = (swiper.realIndex + 1) < 10 ? `0${swiper.realIndex + 1}` : swiper.realIndex + 1;
+	// 			// 	}
+	// 			// }
+	// 		});
+	// 	}
+	// }
 
 
 }
