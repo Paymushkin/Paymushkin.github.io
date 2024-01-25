e48a9ea32bd95912e9d7fb120aa38f2f98bf8b61
Paymushkin <paymushkin_a@bk.ru>
2024-01-25 15:58:22 +0300

    upd

---
+++ sofinet/src/js/files/script.js
@@ -0,0 +1,14 @@
+// Подключение функционала "Чертогов Фрилансера"
+import { isMobile } from "./functions.js";
+// Подключение списка активных модулей
+import { flsModules } from "./modules.js";
+
+const passwordInput = document.getElementById("inputPassword")
+const showHideIcon = document.getElementById("showHideIcon")
+showHideIcon.addEventListener("click", ()=> {
+	if (passwordInput.type !== "text") {
+		passwordInput.type = "text"
+	} else passwordInput.type = "password"
+	
+})
+console.log(passwordInput)
