e0cbb90b3cee87f62e65abbcbe5160bf244410a3
Paymushkin <paymushkin_a@bk.ru>
2024-01-28 15:01:41 +0300

    upd

--- sofinet/src/js/files/script.js
+++ sofinet/src/js/files/script.js
@@ -1,4 +1,26 @@
 // Подключение функционала "Чертогов Фрилансера"
-import { isMobile } from "./functions.js";
+import {
+    isMobile
+} from "./functions.js";
 // Подключение списка активных модулей
-import { flsModules } from "./modules.js";
+import {
+    flsModules
+} from "./modules.js";
+
+// const passwordInput = document.getElementById("inputPassword")
+// const showHideIcon = document.getElementById("showHideIcon")
+// showHideIcon.addEventListener("click", () => {
+//     if (passwordInput.type !== "text") {
+//         passwordInput.type = "text"
+//     } else passwordInput.type = "password"
+// })
+// console.log(passwordInput)
+document.querySelectorAll('input').forEach(input => {
+    input.addEventListener('blur', function () {
+        if (this.value) {
+            this.classList.add('used');
+        } else {
+            this.classList.remove('used');
+        }
+    });
+});
