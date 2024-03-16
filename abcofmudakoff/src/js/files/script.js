ffd7501d3b5053453cb97f46330ee194b307e48a
Paymushkin <paymushkin_a@bk.ru>
2024-03-17 02:27:34 +0300

    upd

---
+++ abcofmudakoff/src/js/files/script.js
@@ -0,0 +1,15 @@
+// Подключение функционала "Чертогов Фрилансера"
+import {
+    isMobile
+} from "./functions.js";
+// Подключение списка активных модулей
+import {
+    flsModules
+} from "./modules.js";
+
+// document.addEventListener("DOMContentLoaded", ()=> {
+//     setTimeout(() => {
+//         document.getElementById("loader").remove()
+//     }, 2000);
+    
+// })
