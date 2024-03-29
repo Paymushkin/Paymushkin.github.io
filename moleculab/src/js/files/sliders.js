ef25f17d39c834c6063ebb3c60b8a79ffab37c97
Paymushkin <paymushkin_a@bk.ru>
2024-03-29 23:46:54 +0300

    upd

--- moleculab/src/js/files/sliders.js
+++ moleculab/src/js/files/sliders.js
@@ -52,7 +52,7 @@ function initSliders() {
 		new Swiper('.news__slider', { // Указываем скласс нужного слайдера
 			// Подключаем модули слайдера
 			// для конкретного случая
-			modules: [Autoplay, Navigation, Pagination],
+			modules: [Autoplay, Pagination],
 			observer: true,
 			observeParents: true,
 			slidesPerView: 1,
@@ -78,11 +78,11 @@ function initSliders() {
 
 			// Пагинация
 
-			// pagination: {
-			// 	el: '.swiper-pagination',
-			// 	clickable: true,
-			// 	type: "fraction",
-			// },
+			pagination: {
+				el: '.news__pagination',
+				clickable: true,
+				// type: "fraction",
+			},
 
 
 			// Скроллбар
@@ -94,10 +94,10 @@ function initSliders() {
 			*/
 
 			// Кнопки "влево/вправо"
-			navigation: {
-				prevEl: '.navigation .prev',
-				nextEl: '.navigation .next',
-			},
+			// navigation: {
+			// 	prevEl: '.navigation .prev',
+			// 	nextEl: '.navigation .next',
+			// },
 
 			// Брейкпоинты
 
@@ -113,6 +113,99 @@ function initSliders() {
 				},
 				992: {
 					slidesPerView: 3,
+					spaceBetween: 20,
+				},
+				1268: {
+					slidesPerView: 4,
+					spaceBetween: 20,
+				},
+			},
+			// События
+			// on: {
+			// 	init: function(swiper) {
+			// 		const allSlides = document.querySelector(".fraction-controll__all");
+			// 		const allSlidesItems = document.querySelectorAll(".slide-main-block:not(.swiper-slide-duplicate)");
+
+			// 		allSlides.textContent = allSlidesItems.length < 10 ? `0${allSlidesItems.length}` : allSlidesItems.length;
+			// 	},
+			// 	slideChange: function(swiper) {
+			// 		const currentSlide = document.querySelector(".fraction-controll__current");
+			// 		currentSlide.textContent = (swiper.realIndex + 1) < 10 ? `0${swiper.realIndex + 1}` : swiper.realIndex + 1;
+			// 	}
+			// }
+		});
+	}
+
+	if (document.querySelector('.spheres__slider')) { // Указываем скласс нужного слайдера
+		// Создаем слайдер
+		new Swiper('.spheres__slider', { // Указываем скласс нужного слайдера
+			// Подключаем модули слайдера
+			// для конкретного случая
+			modules: [Autoplay, Pagination],
+			observer: true,
+			observeParents: true,
+			slidesPerView: 1,
+			spaceBetween: 0,
+			// autoHeight: true,
+			speed: 800,
+
+			//touchRatio: 0,
+			//simulateTouch: false,
+			// loop: true,
+			//preloadImages: false,
+			//lazy: true,
+			// parallax: true,
+
+
+			// Эффекты
+			// effect: 'fade',
+			// autoplay: {
+			// 	delay: 3000,
+			// 	disableOnInteraction: false,
+			// },
+
+
+			// Пагинация
+
+			pagination: {
+				el: '.spheres__pagination',
+				clickable: true,
+				// type: "fraction",
+			},
+
+
+			// Скроллбар
+			/*
+			scrollbar: {
+				el: '.swiper-scrollbar',
+				draggable: true,
+			},
+			*/
+
+			// Кнопки "влево/вправо"
+			// navigation: {
+			// 	prevEl: '.navigation .prev',
+			// 	nextEl: '.navigation .next',
+			// },
+
+			// Брейкпоинты
+
+			breakpoints: {
+				320: {
+					slidesPerView: 2,
+					spaceBetween: 20,
+					autoHeight: true,
+				},
+				576: {
+					slidesPerView: 2,
+					spaceBetween: 20,
+				},
+				768: {
+					slidesPerView: 4,
+					spaceBetween: 10,
+				},
+				992: {
+					slidesPerView: 4,
 					spaceBetween: 20,
 				},
 				1268: {
