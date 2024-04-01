913afe99275450ee68f32257244e9560e51d22a9
Paymushkin <paymushkin_a@bk.ru>
2024-04-01 19:17:49 +0300

    upd

--- moleculab/src/js/files/sliders.js
+++ moleculab/src/js/files/sliders.js
@@ -80,7 +80,7 @@ function initSliders() {
 
 			pagination: {
 				el: '.news__pagination',
-				clickable: true,
+				// clickable: true,
 				// type: "fraction",
 			},
 
@@ -174,7 +174,7 @@ function initSliders() {
 
 			pagination: {
 				el: '.spheres__pagination',
-				clickable: true,
+				// clickable: true,
 				// type: "fraction",
 			},
 
@@ -267,7 +267,7 @@ function initSliders() {
 
 			pagination: {
 				el: '.body-events__pagination',
-				clickable: true,
+				// clickable: true,
 				// type: "fraction",
 			},
 
@@ -308,6 +308,99 @@ function initSliders() {
 				},
 				1268: {
 					slidesPerView: 2,
+					spaceBetween: 20,
+				},
+			},
+			// События
+			// on: {
+			// 	init: function(swiper) {
+			// 		const allSlides = document.querySelector(".fraction-controll__all");
+			// 		const allSlidesItems = document.querySelectorAll(".slide-main-block:not(.swiper-slide-duplicate)");
+
+			// 		allSlides.textContent = allSlidesItems.length < 10 ? `0${allSlidesItems.length}` : allSlidesItems.length;
+			// 	},
+			// 	slideChange: function(swiper) {
+			// 		const currentSlide = document.querySelector(".fraction-controll__current");
+			// 		currentSlide.textContent = (swiper.realIndex + 1) < 10 ? `0${swiper.realIndex + 1}` : swiper.realIndex + 1;
+			// 	}
+			// }
+		});
+	}
+
+	if (document.querySelector('.clients__slider')) { // Указываем скласс нужного слайдера
+		// Создаем слайдер
+		new Swiper('.clients__slider', { // Указываем скласс нужного слайдера
+			// Подключаем модули слайдера
+			// для конкретного случая
+			modules: [Autoplay, Pagination],
+			observer: true,
+			observeParents: true,
+			slidesPerView: 1,
+			spaceBetween: 0,
+			// autoHeight: true,
+			speed: 800,
+
+			//touchRatio: 0,
+			//simulateTouch: false,
+			// loop: true,
+			//preloadImages: false,
+			//lazy: true,
+			// parallax: true,
+
+
+			// Эффекты
+			// effect: 'fade',
+			// autoplay: {
+			// 	delay: 3000,
+			// 	disableOnInteraction: false,
+			// },
+
+
+			// Пагинация
+
+			pagination: {
+				el: '.clients__pagination',
+				// clickable: true,
+				// type: "fraction",
+			},
+
+
+			// Скроллбар
+			/*
+			scrollbar: {
+				el: '.swiper-scrollbar',
+				draggable: true,
+			},
+			*/
+
+			// Кнопки "влево/вправо"
+			// navigation: {
+			// 	prevEl: '.navigation .prev',
+			// 	nextEl: '.navigation .next',
+			// },
+
+			// Брейкпоинты
+
+			breakpoints: {
+				320: {
+					slidesPerView: 2,
+					spaceBetween: 10,
+					autoHeight: true,
+				},
+				576: {
+					slidesPerView: 2,
+					spaceBetween: 10,
+				},
+				768: {
+					slidesPerView: 3,
+					spaceBetween: 10,
+				},
+				992: {
+					slidesPerView: 3,
+					spaceBetween: 10,
+				},
+				1268: {
+					slidesPerView: 6,
 					spaceBetween: 20,
 				},
 			},
